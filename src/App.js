8dc4fdad2e82880d2a93cbd19dc86b9a569cac97
Nikoloz Bokeria <nikolozbokeria19@gmail.com>
2021-09-26 12:07:30 +0400

    feat: setup, navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
+import Navbar from './components/Navbar';
+
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home}></Route>
+            <Route exact path="/login" component={login}></Route>
+            <Route exact path="/signup" component={signup}></Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
